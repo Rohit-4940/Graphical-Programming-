07b7592141b8df7daf5b287d9e0f264a956a0d90
Rohit Chapagain <chapagainrohit4940@gmail.com>
2021-06-07 20:59:57 +0545

    Sixth Commit

---
+++ Assingment/Triangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment
+{
+   class Triangle: Shape
+    {
+        PointF[] point;
+
+        public Triangle()
+        {
+
+        }
+        public Triangle(PointF[] point)
+        {
+            this.point = point;
+        }
+        public Triangle(Color color, int x, int y, PointF[] point) : base(x, y)
+        {
+            this.point = point;
+        }
+         public override void draw(Graphics g, Color c, int thickness)
+        {
+            Pen p = new Pen(c);
+            g.DrawPolygon(p, point);
+        }
+        public override void fill(Graphics g, Color c)
+        {
+            SolidBrush fill = new SolidBrush(c);
+            g.FillPolygon(fill, point);
+        }
+        public void setPoints(PointF[] point)
+        {
+            this.point = point;
+        }
+        public PointF[] getPoint()
+        {
+            return this.point;
+        }
+    }
+}
+
