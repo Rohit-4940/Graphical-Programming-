264d8f00ae59ec78563bb5d76befbc7507427583
Rohit Chapagain <chapagainrohit4940@gmail.com>
2021-06-08 20:35:35 +0545

    Seventh Commit

---
+++ Assingment/ShapeFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assingment
+{
+    public class ShapeFactory
+    {
+        public Shape getShape(string shapeType)
+        {
+            shapeType = shapeType.ToLower().Trim();
+
+            if (shapeType == null)
+            {
+                return null;
+            }
+            else if (shapeType.Equals("circle"))
+            {
+                return new Circle();
+            }
+            else if (shapeType.Equals("rectangle"))
+            {
+                return new Rectangle();
+            }
+            else if (shapeType.Equals("triangle"))
+            {
+                return new Triangle();
+            }
+            else
+            {
+                MessageBox.Show("Factory error: " + shapeType + " does not exist");
+                return null;
+            }
+
+        }
+    }
+}
+
