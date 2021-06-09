e8a5721f0e3d0433fc4ee7893da03c4bc811aa77
Rohit Chapagain <chapagainrohit4940@gmail.com>
2021-06-09 21:13:22 +0545

    Eight Commit

---
+++ Assingment/ShapeFactoryDef.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assingment
+{
+    class ShapeFactoryDef
+    {
+        public bool isCircle(string shape)
+        {
+            if (shape == "circle")
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool isRectangle(string shape)
+        {
+            if (shape == "rectangle")
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool isTriangle(string shape)
+        {
+            if (shape == "triangle")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
